b71d069c5003c56e132d059386cefa4ff30eecec
Albion Berisha <albionberisha66@gmail.com>
2024-05-20 19:20:13 +0200

    Added Database Model , Views and Auth0 Auth

---
+++ UniversumSchoolManagement/Data/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversumSchoolManagement.Data
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; } = new List<Enrollment>();
+    }
+}
---
+++ UniversumSchoolManagement/Models/ClassViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversumSchoolManagement.Models
+{
+    public class ClassViewModel
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public string Time { get; set; }
+        public string LecturerName { get; set; }
+    }
+}
